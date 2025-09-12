12ddc93f76d7055b7e74c61a5c3770e3d8043ba0
construct.d <construct.d@proton.me>
2025-09-12 14:14:17 +0000

    Update url utilities documentation

--- lib/phoenix_kit/utils/routes.ex
+++ lib/phoenix_kit/utils/routes.ex
@@ -1,4 +1,16 @@
 defmodule PhoenixKit.Utils.Routes do
+  @moduledoc """
+  URL utilities for PhoenixKit.
+
+  ## Core Functions
+
+  - `path/1` - Returns an url path with preconfigured prefix.
+  - `url/1` - Returns a full url with preconfigured prefix.
+  """
+
+  @doc """
+  Returns an url path with preconfigured prefix.
+  """
   def path(url_path) do
     if String.starts_with?(url_path, "/") do
       url_prefix = PhoenixKit.Config.get_url_prefix()
@@ -10,6 +22,9 @@ defmodule PhoenixKit.Utils.Routes do
     end
   end
 
+  @doc """
+  Returns a full url with preconfigured prefix.
+  """
   def url(url_path) do
     base_url = PhoenixKit.Config.get_base_url()
     full_path = path(url_path)
