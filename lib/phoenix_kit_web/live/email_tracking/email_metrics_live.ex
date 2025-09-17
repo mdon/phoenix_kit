9c313fffb1092c57691abaa804ffa76264b9e169
construct.d <construct.d@proton.me>
2025-09-17 19:17:50 +0000

    Replace svg icon with a corresponding component icon

--- lib/phoenix_kit_web/live/email_tracking/email_metrics_live.ex
+++ lib/phoenix_kit_web/live/email_tracking/email_metrics_live.ex
@@ -44,6 +44,8 @@ defmodule PhoenixKitWeb.Live.EmailTracking.EmailMetricsLive do
   alias PhoenixKit.Settings
   alias PhoenixKit.Utils.Date, as: UtilsDate
 
+  import PhoenixKitWeb.Components.Core.Icons, only: [icon_arrow_left: 1]
+
   # Auto-refresh every 30 seconds
   @refresh_interval 30_000
 
@@ -205,14 +207,7 @@ defmodule PhoenixKitWeb.Live.EmailTracking.EmailMetricsLive do
             navigate="/phoenix_kit/admin"
             class="btn btn-outline btn-primary btn-sm absolute left-0 top-0 -mb-12"
           >
-            <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10 19l-7-7m0 0l7-7m-7 7h18"
-              />
-            </svg>
+            <.icon_arrow_left />
             Back to Admin
           </.link>
 
